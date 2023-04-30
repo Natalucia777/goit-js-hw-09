6a7d9546a326f8d41e112164c9d49d4e923ae8c8
Natalya Demidenko <natalucia777@ukr.net>
2023-04-30 23:36:42 +0300

    hw-9-1-2

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,23 @@
+import '../css/common.css';
+const bodyDoc = document.querySelector('body');
+const buttonStart = document.querySelector('button[data-start]');
+const buttonStop = document.querySelector('button[data-stop]');
 
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+}
+buttonStop.disabled = true;
+let intervalID = null;
+buttonStart.addEventListener('clisk', buttonStartAction);
+function buttonStartAction() {
+  intervalID = setInterval(bgColor, 1000);
+  function bgColor() {
+    bodyDoc.style.backgroundColor = getRandomHexColor();
+    buttonStart.disabled = true;
+  }
+}
+buttonStop.eddEventListener('click', buttonStopAction);
+function buttonStopAction() {
+  clearInterval(intervalID);
+  buttonStart.disabled = false;
+}
