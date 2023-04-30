3921686262b13878b06024ffb1dff48ee10eec9f
Natalya Demidenko <natalucia777@ukr.net>
2023-04-30 23:58:26 +0300

    hw-9-3-1

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,22 @@
+import Notiflix from 'notiflix';
+
+const formElement = document.querySelector('.form');
+
+
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
+  
   if (shouldResolve) {
     // Fulfill
   } else {
     // Reject
   }
 }
+
+createPromise(2, 1500)
+  .then(({ position, delay }) => {
+    console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
+  })
+  .catch(({ position, delay }) => {
+    console.log(`❌ Rejected promise ${position} in ${delay}ms`);
+  });
