15d88dbde98e52a188544a1e4ed001a269e8238b
Natalya Demidenko <natalucia777@ukr.net>
2023-05-02 14:09:03 +0300

    hw-9-3-promise

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -19,22 +19,28 @@ function createPromise(position, delay) {
         resolve({ position, delay });
       } else { reject({ position, delay }); }
     }, delay);
-  }); } 
+  });
+} 
+  
 const submitHandler = e => {
   e.preventDefault();
   if (!e.target.tagName === 'BUTTON') return;
   const {
     elements: { delay, step, amount },
   } = e.currentTarget;
-delayInp = Number(delay.value);
-stepInp = Number(step.value);
-amountInp = Number(amount.value);
+
+  delayInp = Number(delay.value);
+  stepInp = Number(step.value);
+  amountInp = Number(amount.value);
   for (let i = 1; i <= amountInp; i++) {
-    createPromise(i, delayInp).then(({ position, delay }) => {
-        Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`); })
+    createPromise(i, stepInp).then(({ position, delay }) => {
+      Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
+    })
       .catch(({ position, delay }) => {
         Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
       });
-    delayInp += stepInp; }
-e.currentTarget.reset();
+    delayInp += stepInp;
+  }
+  e.currentTarget.reset();
+};
 formElement.addEventListener('submit', submitHandler);
