ce83c3b7bf73e5c324b1a9b35aad09b7cad31570
Natalya Demidenko <natalucia777@ukr.net>
2023-05-03 10:09:32 +0300

    hw-9-2-14

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -8,13 +8,31 @@ const timerDays = document.querySelector('span[data-days]');
 const timerHours = document.querySelector('span[data-hours]');
 const timerMinutes = document.querySelector('span[data-minutes]');
 const timerSeconds = document.querySelector('span[data-seconds]');
+const inputDate = document.querySelector('input#datetime-picker');
 
-let timerID = null;
 let intervalID = null;
-buttonStart.setAttribute('disabled', true);
-// buttonStart.disabled = true;
-  
+buttonStart.disabled = true;
+buttonStart.addEventListener('click', showTimer);
 
+function showTimer() {
+  buttonStart.disabled = true;
+  inputDate.disabled = true;
+  intervalID = setInterval(() => {
+    if (localStorage.getItem('timer') < 1000) {
+      clearInterval(intervalID);
+      localStorage.removeItem('timer');
+      return;
+    }
+    let dateTimer = localStorage.getItem('timer') - 1000;
+    localStorage.setItem('timer', dateTimer);
+    const addLeadingZero = value => String(value).padStart(2, '0');
+    const { days, hours, minutes, seconds } = convertMs(dateTimer);
+    timerDays.textContent = addLeadingZero(`${days}`);
+    timerHours.textContent = addLeadingZero(`${hours}`);
+    timerMinutes.textContent = addLeadingZero(`${minutes}`);
+    timerSeconds.textContent = addLeadingZero(`${seconds}`);
+  }, 1000);
+}
 const options = {
   enableTime: true,
   time_24hr: true,
@@ -25,10 +43,8 @@ const options = {
       Notiflix.Notify.failure('Please choose a date in the future');
       return;
     } else {
-    buttonStart.removeAttribute('disabled');
-    // buttonStart.disabled = false;
+      buttonStart.removeAttribute('disabled');
     }
-    
     const now = new Date();
     const selectDate = selectedDates[0].getTime();
     const timeNow = now.getTime();
@@ -42,11 +58,8 @@ const options = {
     localStorage.setItem('timer', -start);
   },
 };
-
 flatpickr('#datetime-picker', options);
-
 function convertMs(ms) {
-  // Number of milliseconds per unit of time
   const second = 1000;
   const minute = second * 60;
   const hour = minute * 60;
@@ -64,28 +77,6 @@ function convertMs(ms) {
 
   return { days, hours, minutes, seconds };
 }
-
-
-function showTimer() {
-  intervalID = setInterval(() => {
-    if (localStorage.getItem('timer') < 1000) {
-      clearInterval(intervalID);
-      localStorage.removeItem('timer');
-      return;
-    }
-
-    let dateTimer = localStorage.getItem('timer') - 1000;
-    localStorage.setItem('timer', dateTimer);
-    const addLeadingZero = value => String(value).padStart(2, '0');
-    const { days, hours, minutes, seconds } = convertMs(dateTimer);
-    timerDays.textContent = addLeadingZero(`${days}`);
-    timerHours.textContent = addLeadingZero(`${hours}`);
-    timerMinutes.textContent = addLeadingZero(`${minutes}`);
-    timerSeconds.textContent = addLeadingZero(`${seconds}`);
-  }, 1000);
-buttonStart.addEventListener('click', showTimer);
-}
-
 console.log(convertMs(2000)); // {days: 0, hours: 0, minutes: 0, seconds: 2}
 console.log(convertMs(140000)); // {days: 0, hours: 0, minutes: 2, seconds: 20}
 console.log(convertMs(24140000)); // {days: 0, hours: 6 minutes: 42, seconds: 20}
