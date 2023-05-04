420b1bb1a5cccf88457e09e856a1079ea410d446
Natalya Demidenko <natalucia777@ukr.net>
2023-05-05 01:00:39 +0300

    hw-9-3-5

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -8,11 +8,11 @@ let amountInp = document.querySelector('input[name="amount"]');
 
 formElement.addEventListener('submit', submitHandler);
 
-const submitHandler = e => {
+function submitHandler(e) {
   e.preventDefault();
   let timeDelay = Number(delayInp.value);
-  let timeAmount = Number(amountInp.value);
-  for (let i = 1; i <= timeAmount; i++) {
+  // let timeAmount = Number(amountInp.value);
+  for (let i = 1; i <= Number(amountInp.value); i += 1) {
     createPromise(i, timeDelay)
       .then(({ position, delay }) => {
         Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
@@ -25,13 +25,13 @@ const submitHandler = e => {
   e.currentTarget.reset()
 }
 
-createPromise(2, 1500)
-  .then(({ position, delay }) => {
-    console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
-  })
-  .catch(({ position, delay }) => {
-    console.log(`❌ Rejected promise ${position} in ${delay}ms`);
-  });
+// createPromise(2, 1500)
+//   .then(({ position, delay }) => {
+//     console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
+//   })
+//   .catch(({ position, delay }) => {
+//     console.log(`❌ Rejected promise ${position} in ${delay}ms`);
+//   });
 
 function createPromise(position, delay) {
    const shouldResolve = Math.random() > 0.3;
