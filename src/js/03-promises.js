08301654a66060f9bcc67e0f53eac3f0d27d45e1
Natalya Demidenko <natalucia777@ukr.net>
2023-05-05 01:04:59 +0300

    hw-9-3-6

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -22,19 +22,10 @@ function submitHandler(e) {
       });
     timeDelay += Number(stepInp.value);
   }
-  e.currentTarget.reset()
+  e.currentTarget.reset();
 }
-
-// createPromise(2, 1500)
-//   .then(({ position, delay }) => {
-//     console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
-//   })
-//   .catch(({ position, delay }) => {
-//     console.log(`❌ Rejected promise ${position} in ${delay}ms`);
-//   });
-
 function createPromise(position, delay) {
-   const shouldResolve = Math.random() > 0.3;
+  const shouldResolve = Math.random() > 0.3;
   return new Promise((resolve, reject) => {
     setTimeout(() => {
       if (shouldResolve) {
